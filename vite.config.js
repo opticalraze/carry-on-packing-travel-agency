99cfd1e07fb11a77f3b72ecbe7aee28cb36b57bd
Markus <ModernFuzzBall2@hotmail.com>
2023-05-09 02:56:28 -0400

    Trying to fix the urls

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const ASSET_URL = process.env.ASSET_URL || '';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: ASSET_URL,
+  base: '/carry-on-packing-travel-agency/',
   plugins: [vue()],
 })
