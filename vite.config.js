e33926a03e0819804629d2253aa305822989b17c
Markus <ModernFuzzBall2@hotmail.com>
2023-05-09 02:50:46 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://opticalraze.com/carry-on-packing-travel-agency/',
   plugins: [vue()],
 })
